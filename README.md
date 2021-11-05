ca38251655deaf7d7cbf5dfa50d5cf35a02fb529
Mihir Kelkar <mihirkelkar@mihirs-mbp.lan>
2021-11-05 15:13:36 -0400

    adding this to code

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # recipe-app-api
 Recipe App API Source code
+
+#how to run tests?
+python manage.py test
+This picks up all files that start with test_ and / or are in the test folder.
+All test functions must also start with the prefix test_ 
---
+++ app/core/tests/test_model.py
@@ -0,0 +1,43 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_success(self):
+        """
+        Test creating a new user with an email is succesful
+        """
+        email = 'test@london.com'
+        password = 'Testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """
+        test the email for a new user is normalized
+        """
+        email = "test@GMAIL.com"
+        user = get_user_model().objects.create_user(email, "test123")
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """
+        test creating user with no email raises error
+        """
+        with self.assertRaises(ValueError):
+            user = get_user_model().objects.create_user(None, "test123")
+
+    def test_create_new_super_user(self):
+        """
+        test creating a new superuser
+        """
+        user = get_user_model().objects.create_superuser(
+            "test@gmail.com",
+            "test123"
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
